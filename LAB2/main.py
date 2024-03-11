1db6604c3486f23e9a60389c0b2d33befa042448
Zlata <zlata@172.23.66.181.wi-fi.pjwstk.edu.pl.pjwstk.edu.pl>
2024-03-11 12:58:50 +0100

    added solution to task2 in main.ipynb file

--- LAB2/main.py
+++ LAB2/main.py
@@ -32,10 +32,10 @@ input_numbers = input("Enter a series of space-separated integers: ")
 
 # Convert Input:
 strings = input_numbers.split(' ')
-nums = [int(x) for x in strings]
+numbers_list = [int(x) for x in strings]
 
 # Sorting:
-nums.sort()
+numbers_list.sort()
 
 # Print Output
 print("Sorted list:", numbers_list)
